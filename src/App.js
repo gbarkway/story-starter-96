515468f5cdd8e8a8ba214c43fe91e0b05910d2ab
Greg Barkway <greg.barkway@gmail.com>
2022-02-20 22:39:26 -0800

    Add buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
 import '98.css'
+import next from './next.ogg';
+import prev from './prev.ogg';
 import collections from './scenes';
 
-
 function App() {
-  const [scene, setScene] = useState(collections[0].scenes[0]);
   const [collection, setCollection] = useState(collections[0])
+  const [sceneIndex, setSceneIndex] = useState(0);
+  const [scene, setScene] = useState(collections[0].scenes[sceneIndex]);
+  const nextAudio = useRef();
+  const prevAudio = useRef();
+
+  useEffect(() => {
+    setSceneIndex(0);
+  }, [collection]);
+
+  useEffect(() => {
+    if (collection.scenes[sceneIndex]) {
+      setScene(collection.scenes[sceneIndex])
+    }
+  }, [sceneIndex, collection.scenes]);
+
+  const handleNext = () => {
+    nextAudio.current.play().catch(() => {});
+    setSceneIndex((sceneIndex + 1) % collection.scenes.length)
+  }
 
   const handleSelectChange = useCallback((event) => {
     setCollection(collections.find(sc => sc.name === event.target.value));
   }, []);
 
-  useEffect(() => {
-    setScene(collection.scenes[0])
-  }, [collection])
-
-  useEffect(() => {
-    console.log(scene)
-  }, [scene])
+  const handlePrev = () => {
+    prevAudio.current.play().catch(() => {});
+    if (sceneIndex) {
+      setSceneIndex(sceneIndex - 1)
+    } else {
+      setSceneIndex(collection.scenes.length - 1)
+    }
+  }
 
   return (
     <div className="App">
+      <audio src={next} ref={nextAudio}></audio>
+      <audio src={prev} ref={prevAudio}></audio>
       <audio autoPlay src={scene.sound}>Your browser does not support audio</audio>
       <div className="frame">
         <div className="dropdown">
@@ -36,6 +58,10 @@ function App() {
         </div>
         <div className="title">
           <label>{scene.name}</label>
+        </div>
+        <div className="buttons">
+          <button type="button" id="left" onClick={handlePrev}></button>
+          <button type="button" id="right" onClick={handleNext}></button>
         </div>
       </div>
     </div>
