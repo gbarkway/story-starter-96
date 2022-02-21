666ea070aadca1d584cdd22acac3caf1ef0e1add
Greg Barkway <greg.barkway@gmail.com>
2022-02-20 19:57:06 -0800

    Add dropdown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import '98.css'
 import scenes from './scenes';
 
 
 function App() {
   const [image, setImage] = useState(scenes[0].scenes[3].image);
+
   return (
     <div className="App">
       <div className="frame">
         <div className="dropdown">
-          
+          <select name="sceneCollections">
+            {scenes.map((scene) => (
+              <option value={scene.directory}>{scene.name}</option>
+            ))}
+          </select>
         </div>
         <div className="image">
           <img src={image}></img>
