0368fa09767b139a7f5705e26b3990065f6066d9
Greg Barkway <greg.barkway@gmail.com>
2022-02-27 22:07:05 -0800

    Increase font size

--- src/App.js
+++ src/App.js
@@ -48,14 +48,17 @@ function App() {
 
   return (
     <div className="App">
-      <div id="links">
-        <a href="https://web.archive.org/web/20000815094541/http://www.microsoft.com/kids/creativewriter/" target="_blank">
-          About
-        </a>
-        <span>|</span>
-        <a href="https://twitter.com/gregbarkway" target="_blank">
-          Twitter
-        </a>
+      <div id="topbar">
+        <div id="title">Story Starter 1996</div>
+        <div id="links">
+          <a href="https://web.archive.org/web/20000815094541/http://www.microsoft.com/kids/creativewriter/" target="_blank">
+            About
+          </a>
+          <span>|</span>
+          <a href="https://twitter.com/gregbarkway" target="_blank">
+            Twitter
+          </a>
+        </div>
       </div>
       <div id="bg" className="bg">
         {pastedImages.map((thing) => (
