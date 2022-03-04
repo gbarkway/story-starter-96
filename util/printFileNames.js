16c36bbd3a4d128b6b337292d33076897228bac2
Greg Barkway <greg.barkway@gmail.com>
2022-03-03 23:14:05 -0800

    Accept command line arg in printFileNames

--- util/printFileNames.js
+++ util/printFileNames.js
@@ -1,4 +1,10 @@
-const adventure = require('../src/sceneManifests/mystery.json');
+if (process.argv.length < 3) {
+  console.log('Usage: node printFileNames.js ./path/to/scene/manifest.json')
+  process.exit(1);
+}
+
+const sceneManifestPath = process.argv[2];
+const adventure = require(sceneManifestPath);
 const filename = (name) => name.replace(/[!.&,'-?]/g, '_');
 adventure.scenes.forEach((scene) => {
   console.log(filename(scene));
