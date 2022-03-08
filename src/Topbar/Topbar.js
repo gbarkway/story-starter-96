dfcd1a209d77e4c70988e53a3c35e0da91093747
Greg Barkway <greg.barkway@gmail.com>
2022-03-07 19:28:22 -0800

    Use semantically useful tags in top bar

--- src/Topbar/Topbar.js
+++ src/Topbar/Topbar.js
@@ -2,17 +2,17 @@ import './Topbar.css';
 
 function Topbar() {
   return (
-    <header id="topbar">
-      <div id="title">Story Starter 1996</div>
-      <div id="links">
+    <header className="top-bar">
+      <h1 className="title">Story Starter 1996</h1>
+      <nav className="links">
         <a href="https://web.archive.org/web/20000815094541/http://www.microsoft.com/kids/creativewriter/" target="_blank" rel="noreferrer">
           About
         </a>
-        <span>|</span>
+        <span className="separator"></span>
         <a href="https://twitter.com/gregbarkway" target="_blank" rel="noreferrer">
           Twitter
         </a>
-      </div>
+      </nav>
     </header>
   );
 }
