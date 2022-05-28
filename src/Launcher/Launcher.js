ef1c584dc5946abd2e20e621bd31c2e871689f00
Greg Barkway <greg.barkway@gmail.com>
2022-05-28 10:03:13 -0700

    Update launcher label

--- src/Launcher/Launcher.js
+++ src/Launcher/Launcher.js
@@ -4,10 +4,12 @@ import '../sprite-button.css';
 function Launcher({ showLabel = true, onClick = (f) => f }) {
   return (
     <div className="launcher">
-      <button aria-labelledby="launch-label" className="sprite-button launch-button" type="button" title="Inspiration" onClick={onClick}></button>
-      <span id="launch-label" className="launch-label" hidden={!showLabel}>
+      <button id="launch-button" className="sprite-button launch-button" type="button" title="Inspiration" onClick={onClick}></button>
+      <label htmlFor="launch-button" className="launch-label" hidden={!showLabel}>
         Click for inspiration
-      </span>
+        <br />
+        (Turn sound on)
+      </label>
     </div>
   );
 }
